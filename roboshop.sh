c57cc615e9f0abad9622a1da14e2977c9385a2f3
linu-2208 <linithakadiyala@gmail.com>
2025-01-15 16:12:06 +0530

    roboshop shell

--- roboshop.sh
+++ roboshop.sh
@@ -16,7 +16,7 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
 
-    aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" 
+    aws ec2 run-instances --image-id ami-0b4f379183e5706b9  --instance-type $INSTANCE_TYPE --security-group-ids sg-07078f1c35acb359b --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" 
     echo "$i: $IP_ADDRESS"
 
 
