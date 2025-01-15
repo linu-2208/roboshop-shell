81bfccce41dceaf4bc0d390fb5930a4dc84b98a9
linu-2208 <linithakadiyala@gmail.com>
2025-01-15 17:36:41 +0530

    roboshop shell

--- roboshop.sh
+++ roboshop.sh
@@ -16,6 +16,27 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
 
-    aws ec2 run-instances --image-id ami-0b4f379183e5706b9  --instance-type $INSTANCE_TYPE --security-group-ids sg-07078f1c35acb359b --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" 
+   IP_ADDRESS= $( aws ec2 run-instances --image-id ami-0b4f379183e5706b9  --instance-type $INSTANCE_TYPE --security-group-ids sg-07078f1c35acb359b --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
 
+    echo "$i: $IP_ADDRESS"
+
+#create R53 record, make sure you delete existing record
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Creating a record set for cognito endpoint"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$i'.'$DOMAIN_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP_ADDRESS'"
+            }]
+        }
+        }]
+    }
+        '
 done
