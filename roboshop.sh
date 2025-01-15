6a1c0719b83cd3f471f75ba9aae6c852b5502cea
linu-2208 <linithakadiyala@gmail.com>
2025-01-15 16:01:57 +0530

    shell

---
+++ roboshop.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+AMI=ami-0b4f379183e5706b9
+SG_ID=sg-07078f1c35acb359b
+ZONE_ID=Z01032471U3T3F8BDO6LI
+DOMAIN_NAME=myver.shop
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+
+for i in "${INSTANCES[@]}"
+do 
+    echo "instance is: $i"
+    if [ $i == mongodb] || [ $i == "mysql"] || [ $i == shipping]
+    then    
+        INSTANCE_TYPE="t3.small"
+    else
+        INSTANCE_TYPE="t2.micro"
+    fi
+
+    aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" 
+    echo "$i: $IP_ADDRESS"
+
+
+done
